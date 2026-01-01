66d570b8aca238e05b617cf720cc9f8266ec78af
rozafebulliqi-bit <rozafe.bulliqi@student.uni-pr.edu>
2026-01-01 21:09:44 +0100

    Enhance output formatting and refactor conditionals
    Added input formatting to display two decimal places for area calculations. Improved structure of conditional statements for clarity.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>   // NEW: for formatting output
 using namespace std;
 
 int main() {
@@ -15,13 +16,16 @@ int main() {
     cout << "Enter your choice :: ";
     cin >> choice;
 
+    cout << fixed << setprecision(2); // NEW: show 2 decimal places
+
     switch (choice) {
         case 1:
             cout << "Enter the radius of the Circle :: ";
             cin >> radius;
-            if (radius > 0)
+            if (radius > 0) {
                 area = M_PI * pow(radius, 2);
-            else {
+                cout << "\nShape :: Circle" << endl;
+            } else {
                 cout << "Invalid radius!" << endl;
                 return 0;
             }
@@ -32,9 +36,10 @@ int main() {
             cin >> length;
             cout << "Enter the width of the Rectangle :: ";
             cin >> width;
-            if (length > 0 && width > 0)
+            if (length > 0 && width > 0) {
                 area = length * width;
-            else {
+                cout << "\nShape :: Rectangle" << endl;
+            } else {
                 cout << "Invalid dimensions!" << endl;
                 return 0;
             }
@@ -43,9 +48,10 @@ int main() {
         case 3:
             cout << "Enter the side length of the Square :: ";
             cin >> length;
-            if (length > 0)
+            if (length > 0) {
                 area = pow(length, 2);
-            else {
+                cout << "\nShape :: Square" << endl;
+            } else {
                 cout << "Invalid length!" << endl;
                 return 0;
             }
@@ -60,6 +66,7 @@ int main() {
             return 0;
     }
 
-    cout << "\nArea :: " << area << endl;
+    cout << "Area :: " << area << endl;
     return 0;
 }
+
